bfabafc819bea3683fbf52ecd5e93bfec2c47f20
Jakub Sikora <jakub.sikora.en@gmail.com>
2014-12-06 20:10:00 +0000

    new build

--- public/dist/application.js
+++ public/dist/application.js
@@ -99,296 +99,336 @@ angular.module('books').config(['$stateProvider',
 
 // Books controller
 angular.module('books').controller('BooksController', ['$scope', '$timeout', '$stateParams', '$location', '$modal', '$log', '$translate', 'Authentication', 'Shelves', 'Genres', 'BooksAPI',
-	function($scope, $timeout, $stateParams, $location, $modal, $log, $translate, Authentication, Shelves, Genres, BooksAPI) {
-		$scope.authentication = Authentication;
-		$scope.shelves = Shelves.query();
-		$scope.genres = Genres.query();
+  function($scope, $timeout, $stateParams, $location, $modal, $log, $translate, Authentication, Shelves, Genres, BooksAPI) {
+    $scope.authentication = Authentication;
+    $scope.shelves = Shelves.query();
+    $scope.genres = Genres.query();
 
-		$scope.data = {
-			shelves: [],
-			alert: null,
-			search: {
-				books: [],
-				hide: true,
-				start: false
-			}
-		};
+    $scope.data = {
+      shelves: [],
+      alert: null,
+      flattenBooks: [],
+      filteredBooks: [],
+      search: {
+        books: [],
+        hide: true,
+        start: false
+      }
+    };
 
-		// Create new Book
-		$scope.create = function(data) {
-			Shelves.get({ shelfId: data.shelf._id }).$promise.then(function(shelf) {
-				var book = {
-					isbn: data.isbn,
-					title: data.title,
-					author: data.author,
-					publishedDate: data.publishedDate,
-					description: data.description,
-					pageCount: data.pageCount,
-					thumbnail: data.thumbnail,
-					coverColour: data.coverColour,
-					fontColour: data.fontColour,
-					genre: data.genre[0]._id
-				};
+    $scope.pagination = {
+      currentPage: 1,
+      maxSize: 5,
+      itemsPerPage: 20
+    };
 
-				shelf.$createBook(book,
-					function(response) {
-						$scope.find();
+    // Create new Book
+    $scope.create = function(data) {
+      Shelves.get({ shelfId: data.shelf._id }).$promise.then(function(shelf) {
+        var book = {
+          isbn: data.isbn,
+          title: data.title,
+          author: data.author,
+          publishedDate: data.publishedDate,
+          description: data.description,
+          pageCount: data.pageCount,
+          thumbnail: data.thumbnail,
+          coverColour: data.coverColour,
+          fontColour: data.fontColour,
+          genre: data.genre[0]._id
+        };
 
-						// TODO: service for alerts
-						$translate('Book has been created.').then(function (alert) {
-					    $scope.data.alert = alert;
+        shelf.$createBook(book,
+          function(response) {
+            $scope.find();
 
-					    $timeout(function() {
-		            $scope.data.alert = null;
-		          }, 3000);
-					  });
-					},
-					function(errorResponse) {
-						$scope.error = errorResponse.data.message;
-					});
-			});
-		};
+            // TODO: service for alerts
+            $translate('Book has been created.').then(function (alert) {
+              $scope.data.alert = alert;
 
-		// Remove existing Book
-		$scope.remove = function(data) {
-			data.shelf.$deleteBook({ 'bookId': data._id},
-				function(response) {
-					$scope.find();
-					// TODO: service for alerts
-					$translate('Book has been deleted.').then(function (alert) {
-				    $scope.data.alert = alert;
+              $timeout(function() {
+                $scope.data.alert = null;
+              }, 3000);
+            });
+          },
+          function(errorResponse) {
+            $scope.error = errorResponse.data.message;
+          });
+      });
+    };
 
-				    $timeout(function() {
-	            $scope.data.alert = null;
-	          }, 3000);
-				  });
-				},
-				function(errorResponse) {
-					$scope.error = errorResponse.data.message;
-			});
-		};
+    // Remove existing Book
+    $scope.remove = function(data) {
+      Shelves.get({ shelfId: data.shelf._id }).$promise.then(function(shelf) {
+        shelf.$deleteBook({ 'bookId': data._id},
+          function(response) {
+            $scope.find();
+            // TODO: service for alerts
+            $translate('Book has been deleted.').then(function (alert) {
+              $scope.data.alert = alert;
 
-		// Update existing Book
-		$scope.update = function(data) {
-			Shelves.get({ shelfId: data.shelf._id }).$promise.then(function(shelf) {
+              $timeout(function() {
+                $scope.data.alert = null;
+              }, 3000);
+            });
+          },
+          function(errorResponse) {
+            $scope.error = errorResponse.data.message;
+        });
+      });
+    };
 
-				// Get shelf for this book to check if it shelf hasnt changed
-				Shelves.byBook({ bookId: data._id}).$promise.then(function(currentShelf) {
-					if (shelf._id !== currentShelf[0]._id) {
-						// Shelf has changed
-						// Delete the book from the currentShelf
-						var currentData = {};
-						currentData = angular.copy(data);
-						currentData.shelf = currentShelf[0];
+    // Update existing Book
+    $scope.update = function(data) {
+      Shelves.get({ shelfId: data.shelf._id }).$promise.then(function(shelf) {
 
-						$scope.remove(currentData);
+        // Get shelf for this book to check if it shelf hasnt changed
+        Shelves.byBook({ bookId: data._id}).$promise.then(function(currentShelf) {
+          if (shelf._id !== currentShelf[0]._id) {
+            // Shelf has changed
+            // Delete the book from the currentShelf
+            var currentData = {};
+            currentData = angular.copy(data);
+            currentData.shelf = currentShelf[0];
 
-						// Create book in the shelf
-						$scope.create(data);
-					} else {
-						shelf.$updateBook({ 'bookId': data._id,
-							isbn: data.isbn,
-							title: data.title,
-							author: data.author,
-							publishedDate: data.publishedDate,
-							description: data.description,
-							pageCount: data.pageCount,
-							thumbnail: data.thumbnail,
-							coverColour: data.coverColour,
-							fontColour: data.fontColour,
-							genre: data.genre[0]._id
-						},
-						function(response) {
-							$scope.find();
+            $scope.remove(currentData);
 
-							// TODO: service for alerts
-							$translate('Book has been updated.').then(function (alert) {
-						    $scope.data.alert = alert;
+            // Create book in the shelf
+            $scope.create(data);
+          } else {
+            shelf.$updateBook({ 'bookId': data._id,
+              isbn: data.isbn,
+              title: data.title,
+              author: data.author,
+              publishedDate: data.publishedDate,
+              description: data.description,
+              pageCount: data.pageCount,
+              thumbnail: data.thumbnail,
+              coverColour: data.coverColour,
+              fontColour: data.fontColour,
+              genre: data.genre[0]._id
+            },
+            function(response) {
+              $scope.find();
 
-						    $timeout(function() {
-			            $scope.data.alert = null;
-			          }, 3000);
-						  });
-						},
-						function(errorResponse) {
-							$scope.error = errorResponse.data.message;
-						});
-		      }
-	      });
-			});
-		};
+              // TODO: service for alerts
+              $translate('Book has been updated.').then(function (alert) {
+                $scope.data.alert = alert;
 
-		// Find a list of Books
-		$scope.find = function() {
-			// Shelves.readBooks().$promise.then(function(books) {
-			// 	$scope.data.books = books;
-			// });
+                $timeout(function() {
+                  $scope.data.alert = null;
+                }, 3000);
+              });
+            },
+            function(errorResponse) {
+              $scope.error = errorResponse.data.message;
+            });
+          }
+        });
+      });
+    };
 
-			Shelves.query().$promise.then(function(shelves) {
-      	$scope.data.shelves = shelves;
-    	});
-		};
+    // Find a list of Books
+    $scope.find = function() {
+      var newBook = {};
 
-		$scope.addShelf = function() {
-			Shelves.save({}, {name: $scope.formData.newshelf}, function(response) {
-				$scope.shelves = Shelves.query(function(shelf) {
-					// Preselect shelf with newly created item
-					$scope.formData.shelf = shelf[0];
-				});
-			}, function(errorResponse) {
-				$scope.errorResponse = errorResponse.data.message;
-			});
-		};
+      Shelves.query().$promise.then(function(shelves) {
+        $scope.data.flattenBooks = [];
 
-		$scope.addGenre = function() {
-			Genres.save({}, {name: $scope.formData.newgenre}, function(response) {
-				$scope.genres = Genres.query(function(genres) {
-					// Preselect genre with newly created item
-					$scope.formData.genre[0] = genres[0];
-				});
-			}, function(errorResponse) {
-				$scope.errorResponse = errorResponse.data.message;
-			});
-		};
+        shelves.forEach(function(shelf) {
+          if (shelf.books.length > 0) {
+            shelf.books.forEach(function(book) {
+              newBook = {
+                shelf: {
+                  _id: shelf._id,
+                  name: shelf.name
+                }
+              };
 
-		$scope.modalUpdate = function (size, selectedBook) {
-			var modalInstance = $modal.open({
-	      templateUrl: 'modules/books/views/edit-book.client.view.html',
-	      controller: ["$scope", "$modalInstance", "book", function($scope, $modalInstance, book) {
-	      	Shelves.byBook({ bookId: book._id}, function(shelf) {
-	      		$scope.formData = book;
-						$scope.formData.shelf = shelf[0];
-	      	});
+              angular.extend(newBook, book);
+              $scope.data.flattenBooks.push(newBook);
+            });
+          }
 
-	      	$scope.ok = function () {
-				    if (this.form.editBookForm.$valid) {
-	      			$modalInstance.close($scope.formData);
-      			} else {
-      				$translate('Please fill or required fields').then(function (requiredFields) {
-						    $scope.error = requiredFields;
-						  });
-      			}
-				  };
+          $scope.pagination.totalItems = $scope.data.flattenBooks.length;
+        });
 
-				  $scope.cancel = function () {
-				    $modalInstance.dismiss('cancel');
-				  };
-	      }],
-	      size: size,
-	      resolve: {
-	        book: function () {
-	        	return selectedBook;
-	        }
-	      }
-	    });
+        $scope.pageChanged();
+      });
+    };
 
-	    modalInstance.result.then(function (formData) {
-	      $scope.update(formData);
-	    }, function () {
-	      $log.info('Modal dismissed at: ' + new Date());
-	    });
-	  };
+    $scope.pageChanged = function() {
+      var begin = (($scope.pagination.currentPage - 1) * $scope.pagination.itemsPerPage)
+        , end = begin + $scope.pagination.itemsPerPage;
 
-	  $scope.modalDelete = function (size, selectedShelf, selectedBook) {
-	    var modalInstance = $modal.open({
-	      templateUrl: 'modules/books/views/delete-book.client.view.html',
-	      controller: ["$scope", "$modalInstance", "book", "shelf", function($scope, $modalInstance, book, shelf) {
-	      	$scope.formData = book;
-	      	$scope.formData.shelf = shelf;
+      var firstOnPage = 1 + begin;
+      var lastOnPage = firstOnPage + $scope.pagination.itemsPerPage - 1;
+      if (lastOnPage > $scope.data.flattenBooks.length) {
+        lastOnPage = $scope.data.flattenBooks.length;
+      }
 
-	      	$scope.ok = function () {
-				    $modalInstance.close($scope.formData);
-				  };
+      $scope.pagination.firstOnPage = firstOnPage;
+      $scope.pagination.lastOnPage = lastOnPage;
 
-				  $scope.cancel = function () {
-				    $modalInstance.dismiss('cancel');
-				  };
-	      }],
-	      size: size,
-	      resolve: {
-	        book: function () {
-	          return selectedBook;
-	        },
-	        shelf: function () {
-	          return selectedShelf;
-	        }
-	      }
-	    });
+      $scope.data.filteredBooks = $scope.data.flattenBooks.slice(begin, end);
+    };
 
-	    modalInstance.result.then(function (formData) {
-	    	$scope.remove(formData);
-	    }, function () {
-	      $log.info('Modal dismissed at: ' + new Date());
-	    });
-	  };
+    $scope.addShelf = function() {
+      Shelves.save({}, {name: $scope.formData.newshelf}, function(response) {
+        $scope.shelves = Shelves.query(function(shelf) {
+          // Preselect shelf with newly created item
+          $scope.formData.shelf = shelf[0];
+        });
+      }, function(errorResponse) {
+        $scope.errorResponse = errorResponse.data.message;
+      });
+    };
 
-		$scope.modalCreate = function (size) {
-	    var modalInstance = $modal.open({
-	      templateUrl: 'modules/books/views/create-book.client.view.html',
-	      controller: ["$scope", "$modalInstance", function($scope, $modalInstance) {
-	      	$scope.formData = { };
-	      	$scope.formData.fontColour = '#FFFFFF';
-	      	$scope.formData.coverColour = '#000000';
+    $scope.addGenre = function() {
+      Genres.save({}, {name: $scope.formData.newgenre}, function(response) {
+        $scope.genres = Genres.query(function(genres) {
+          // Preselect genre with newly created item
+          $scope.formData.genre[0] = genres[0];
+        });
+      }, function(errorResponse) {
+        $scope.errorResponse = errorResponse.data.message;
+      });
+    };
 
-	      	$scope.ok = function () {
-	      		if (this.form.createBookForm.$valid) {
-	      			$modalInstance.close($scope.formData);
-      			} else {
-      				$translate('Please fill or required fields').then(function (requiredFields) {
-						    $scope.error = requiredFields;
-						  });
-      			}
-				  };
+    $scope.modalUpdate = function (size, selectedBook) {
+      var modalInstance = $modal.open({
+        templateUrl: 'modules/books/views/edit-book.client.view.html',
+        controller: ["$scope", "$modalInstance", "book", function($scope, $modalInstance, book) {
+          Shelves.byBook({ bookId: book._id}, function(shelf) {
+            $scope.formData = book;
+            $scope.formData.shelf = shelf[0];
+          });
 
-				  $scope.cancel = function () {
-				    $modalInstance.dismiss('cancel');
-				  };
-	      }],
-	      size: size
-	    });
+          $scope.ok = function () {
+            if (this.form.editBookForm.$valid) {
+              $modalInstance.close($scope.formData);
+            } else {
+              $translate('Please fill or required fields').then(function (requiredFields) {
+                $scope.error = requiredFields;
+              });
+            }
+          };
 
-	    modalInstance.result.then(function (formData) {
-    		$scope.create(formData);
-	    }, function () {
-	      $log.info('Modal dismissed at: ' + new Date());
-	    });
-	  };
+          $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+          };
+        }],
+        size: size,
+        resolve: {
+          book: function () {
+            return selectedBook;
+          }
+        }
+      });
 
-	  $scope.executeSearch = function(subQuery) {
-	  	$scope.data.search.start = true;
-			BooksAPI.searchBooks(subQuery + $scope.formData.isbn, function (error, data) {
+      modalInstance.result.then(function (formData) {
+        $scope.update(formData);
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.modalDelete = function (size, selectedBook) {
+      var modalInstance = $modal.open({
+        templateUrl: 'modules/books/views/delete-book.client.view.html',
+        controller: ["$scope", "$modalInstance", "book", function($scope, $modalInstance, book) {
+          $scope.formData = book;
+
+          $scope.ok = function () {
+            $modalInstance.close($scope.formData);
+          };
+
+          $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+          };
+        }],
+        size: size,
+        resolve: {
+          book: function () {
+            return selectedBook;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (formData) {
+        $scope.remove(formData);
+      }, function () {
+        //$log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.modalCreate = function (size) {
+      var modalInstance = $modal.open({
+        templateUrl: 'modules/books/views/create-book.client.view.html',
+        controller: ["$scope", "$modalInstance", function($scope, $modalInstance) {
+          $scope.formData = { };
+          $scope.formData.fontColour = '#FFFFFF';
+          $scope.formData.coverColour = '#000000';
+
+          $scope.ok = function () {
+            if (this.form.createBookForm.$valid) {
+              $modalInstance.close($scope.formData);
+            } else {
+              $translate('Please fill or required fields').then(function (requiredFields) {
+                $scope.error = requiredFields;
+              });
+            }
+          };
+
+          $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+          };
+        }],
+        size: size
+      });
+
+      modalInstance.result.then(function (formData) {
+        $scope.create(formData);
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.executeSearch = function(subQuery) {
+      $scope.data.search.start = true;
+      BooksAPI.searchBooks(subQuery + $scope.formData.isbn, function (error, data) {
         if (!error) {
-        	$scope.data.search.hide = false;
-        	$scope.data.search.books = data.items ? data.items : [];
+          $scope.data.search.hide = false;
+          $scope.data.search.books = data.items ? data.items : [];
         }
         $scope.data.search.start = false;
-	    });
-		};
+      });
+    };
 
-		$scope.closeSearch = function() {
-			$scope.data.search.hide = true;
-			$scope.data.search.books = [];
-		};
+    $scope.closeSearch = function() {
+      $scope.data.search.hide = true;
+      $scope.data.search.books = [];
+    };
 
-		$scope.loadBook = function(book) {
-			var bookInfo = book.volumeInfo;
-			$scope.formData.title = bookInfo.title || '';
-			$scope.formData.author = bookInfo.authors ? bookInfo.authors.join(', ') : '';
-			$scope.formData.pageCount = bookInfo.pageCount || 0;
-			$scope.formData.publishedDate = bookInfo.publishedDate || '';
-			$scope.formData.thumbnail = bookInfo.imageLinks ? bookInfo.imageLinks.smallThumbnail : '';
-		};
+    $scope.loadBook = function(book) {
+      var bookInfo = book.volumeInfo;
+      $scope.formData.title = bookInfo.title || '';
+      $scope.formData.author = bookInfo.authors ? bookInfo.authors.join(', ') : '';
+      $scope.formData.pageCount = bookInfo.pageCount || 0;
+      $scope.formData.publishedDate = bookInfo.publishedDate || '';
+      $scope.formData.thumbnail = bookInfo.imageLinks ? bookInfo.imageLinks.smallThumbnail : '';
+    };
 
-		$scope.format = function(field) {
-			// TODO:
-			if (field === 'isbn') {
-				$scope.formData[field] = $scope.formData[field].replace(/-/g, '');
-			}
-		};
+    $scope.format = function(field) {
+      // TODO:
+      if (field === 'isbn') {
+        $scope.formData[field] = $scope.formData[field].replace(/-/g, '');
+      }
+    };
 
-	  // Load books
-	  $scope.find();
-	}
+    // Load books
+    $scope.find();
+  }
 ]);
 'use strict';
 
