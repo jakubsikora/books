5bc9bf069c665a4ae1d70668ed2fb5f43e99aa31
Jakub Sikora <jakub.sikora.en@gmail.com>
2017-02-12 14:54:28 +0000

    mongo

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var init = require('./config/init')(),
  */
 
 // Bootstrap db connection
+console.log('Connecting to: ' + config.db);
 var db = mongoose.connect(config.db, function(err) {
 	if (err) {
 		console.error('\x1b[31m', 'Could not connect to MongoDB!');
