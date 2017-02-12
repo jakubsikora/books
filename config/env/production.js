d5d9f02a318f1e8abf4415ed34619c0e4ed98f4b
Jakub Sikora <jakub.sikora.en@gmail.com>
2017-02-12 11:48:04 +0000

    prod config

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/mybooks',
+	db: process.env.MONGO_URL,
 
 	assets: {
 		lib: {
